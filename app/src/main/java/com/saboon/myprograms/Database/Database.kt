fb88b091d19cdd51da7e75fe859b96d415bb9638
Sabahattin Çiçek <scicek12345@gmail.com>
2022-05-29 14:56:01 +0300

    created Database.kt

--- app/src/main/java/com/saboon/myprograms/Database/Database.kt
+++ app/src/main/java/com/saboon/myprograms/Database/Database.kt
@@ -1,6 +1,9 @@
 package com.saboon.myprograms.Database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.saboon.myprograms.Models.ModelProgram
 import com.saboon.myprograms.Models.ModelStates
 import com.saboon.myprograms.Models.ModelSubject
@@ -8,6 +11,33 @@ import com.saboon.myprograms.Models.ModelSubjectTime
 
 
 @Database(entities = arrayOf(ModelProgram::class, ModelSubject::class, ModelSubjectTime::class, ModelStates::class), version = 1)
-class Database {
+abstract class Database: RoomDatabase() {
+
+
+    abstract fun programDAO() : ProgramDAO
+
+
+    //\\--SINGLETON--//\\
+
+    companion object{
+        @Volatile
+        private var instance : com.saboon.myprograms.Database.Database? = null
+
+        private val lock = Any()
+        operator fun invoke(context: Context) = instance ?: synchronized(lock){
+            instance ?: makeDatabase(context).also {
+                instance = it
+            }
+        }
+
+
+
+        private fun makeDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            com.saboon.myprograms.Database.Database::class.java, "MyProgramsDatabase"
+        ).build()
+
+        //.enableMultiInstanceInvalidation()
+    }
 
 }
---
+++ app/src/main/java/com/saboon/myprograms/Database/SubjectDAO.kt
@@ -0,0 +1,11 @@
+package com.saboon.myprograms.Database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import com.saboon.myprograms.Models.ModelSubject
+
+
+@Dao
+interface SubjectDAO {
+
+}
