26840d56a5d5c21dd51f639958e013af829f53a1
Sabahattin Çiçek <scicek12345@gmail.com>
2022-05-29 18:04:36 +0300

    done ManageProgramsFragment.kt

--- app/src/main/java/com/saboon/myprograms/Adapters/ManageProgramsFragmentRecyclerAdapter.kt
+++ app/src/main/java/com/saboon/myprograms/Adapters/ManageProgramsFragmentRecyclerAdapter.kt
@@ -1,25 +1,50 @@
 package com.saboon.myprograms.Adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.saboon.myprograms.Fragments.ManageProgramsFragmentDirections
+import com.saboon.myprograms.Models.ModelProgram
+import com.saboon.myprograms.R
+import java.text.SimpleDateFormat
 
-class ManageProgramsFragmentRecyclerAdapter:RecyclerView.Adapter<ManageProgramsFragmentRecyclerAdapter.ManageProgramViewHolder>() {
+class ManageProgramsFragmentRecyclerAdapter(val programList: List<ModelProgram>):RecyclerView.Adapter<ManageProgramsFragmentRecyclerAdapter.ManageProgramViewHolder>() {
 
 
     class ManageProgramViewHolder(view: View):RecyclerView.ViewHolder(view) {
-
+        val programName: TextView = view.findViewById(R.id.editText_programName)
+        val editedDate: TextView = view.findViewById(R.id.textView_dateEdited)
+        val goToDetails: ImageView = view.findViewById(R.id.imageView_goToDetails)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ManageProgramViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_row_manage_program,parent,false)
+        return ManageProgramViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ManageProgramViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.programName.text = programList[position].name
+
+        //gelen tarih verisi soyle dd.MM.yyyy-HH:mm:ss
+        //sadece tarih kismini almak icin parcalayip dd MMMM yyyy formatina ceviriyoruz
+
+        val dateEdited = SimpleDateFormat("dd MMMM yyyy")
+            .format(SimpleDateFormat("dd.MM.yyyy")
+                .parse(programList[position].dateEdited.split("-")[0]))
+
+        holder.editedDate.text = dateEdited
+
+        holder.itemView.setOnClickListener {
+            val action = ManageProgramsFragmentDirections.actionManageProgramsFragmentToDetailsProgramFragment()
+            it.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return programList.size
     }
 }
--- app/src/main/java/com/saboon/myprograms/Fragments/ManageProgramsFragment.kt
+++ app/src/main/java/com/saboon/myprograms/Fragments/ManageProgramsFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.saboon.myprograms.Adapters.ManageProgramsFragmentRecyclerAdapter
+import com.saboon.myprograms.Models.ModelProgram
 import com.saboon.myprograms.Utils.FROM_MANAGE_PROGRAMS
+import com.saboon.myprograms.ViewModels.ManageProgramsViewModel
 import com.saboon.myprograms.databinding.FragmentManageProgramsBinding
 
 class ManageProgramsFragment : Fragment() {
@@ -15,6 +21,9 @@ class ManageProgramsFragment : Fragment() {
     private val binding get() = _binding!!
 
 
+    lateinit var viewModel: ManageProgramsViewModel
+
+    lateinit var programList: List<ModelProgram>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,9 +45,52 @@ class ManageProgramsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this).get(ManageProgramsViewModel::class.java)
+        viewModel.getAllPrograms()
 
+        observer()
+
+        binding.recyclerViewManageProgramsFragment.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewManageProgramsFragment.adapter = ManageProgramsFragmentRecyclerAdapter(programList)
 
         buttons()
+    }
+
+    fun observer(){
+
+        viewModel.states.observe(viewLifecycleOwner, Observer {
+            it.let {
+                if (it.loading){
+                    binding.recyclerViewManageProgramsFragment.visibility = View.GONE
+                    binding.progressBarLoading.visibility = View.VISIBLE
+                    binding.linearLayoutEmpty.visibility = View.GONE
+                    binding.linearLayoutError.visibility = View.GONE
+                }
+                else if (it.empty){
+                    binding.recyclerViewManageProgramsFragment.visibility = View.GONE
+                    binding.progressBarLoading.visibility = View.GONE
+                    binding.linearLayoutEmpty.visibility = View.VISIBLE
+                    binding.linearLayoutError.visibility = View.GONE
+                }
+                else if (it.error){
+                    binding.recyclerViewManageProgramsFragment.visibility = View.GONE
+                    binding.progressBarLoading.visibility = View.GONE
+                    binding.linearLayoutEmpty.visibility = View.GONE
+                    binding.linearLayoutError.visibility = View.VISIBLE
+                }
+                else{
+                    viewModel.programs.observe(viewLifecycleOwner, Observer {
+                        it.let {
+                            programList = it!!
+                            binding.recyclerViewManageProgramsFragment.visibility = View.VISIBLE
+                            binding.progressBarLoading.visibility = View.GONE
+                            binding.linearLayoutEmpty.visibility = View.GONE
+                            binding.linearLayoutError.visibility = View.GONE
+                        }
+                    })
+                }
+            }
+        })
     }
 
 
