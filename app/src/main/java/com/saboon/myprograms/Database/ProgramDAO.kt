e213b782b59664addfb80b8a0e805d6131e1396e
Sabahattin Çiçek <scicek12345@gmail.com>
2022-05-29 14:49:53 +0300

    created ProgramDAO.kt

---
+++ app/src/main/java/com/saboon/myprograms/Database/ProgramDAO.kt
@@ -0,0 +1,29 @@
+package com.saboon.myprograms.Database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.saboon.myprograms.Models.ModelProgram
+
+
+@Dao
+interface ProgramDAO {
+
+    @Insert
+    suspend fun insertProgram(program: ModelProgram)
+
+    @Query("SELECT * FROM ModelProgram")
+    suspend fun getAllProgram(): List<ModelProgram>
+
+    @Query("SELECT * FROM ModelProgram WHERE name LIKE :filter")
+    suspend fun getAllProgramByFilter(filter: String): List<ModelProgram>
+
+    @Query("SELECT * FROM ModelProgram WHERE id = :programID")
+    suspend fun getProgram(programID:String):ModelProgram
+
+    @Query("UPDATE ModelProgram SET name = :newName , dateEdited = :newDateEdited WHERE id = :programID")
+    suspend fun updateProgram(programID: String, newName: String, newDateEdited: String)
+
+    @Query("DELETE FROM ModelProgram WHERE id = :programID")
+    suspend fun deleteProgram(programID: String)
+}
