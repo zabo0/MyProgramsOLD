0680bd10f02be8da28c9c94c1cbc76e14cd742e4
Sabahattin Çiçek <scicek12345@gmail.com>
2022-05-29 15:29:02 +0300

    created and designed ManageProgramsFragmentRecyclerAdapter.kt

---
+++ app/src/main/java/com/saboon/myprograms/Adapters/ManageProgramsFragmentRecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.saboon.myprograms.Adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ManageProgramsFragmentRecyclerAdapter:RecyclerView.Adapter<ManageProgramsFragmentRecyclerAdapter.ManageProgramViewHolder>() {
+
+
+    class ManageProgramViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ManageProgramViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ManageProgramViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
