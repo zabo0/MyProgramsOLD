6567b7db7d6c1be36f0d04fa29bf5d8a355cd864
Sabahattin Çiçek <scicek12345@gmail.com>
2022-05-30 12:49:19 +0300

    created SubjectTimeDAO.kt

---
+++ app/src/main/java/com/saboon/myprograms/Database/SubjectTimeDAO.kt
@@ -0,0 +1,47 @@
+package com.saboon.myprograms.Database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.saboon.myprograms.Models.ModelSubjectTime
+
+@Dao
+interface SubjectTimeDAO {
+
+    @Insert
+    suspend fun insert(subjectTime: ModelSubjectTime)
+
+    @Query("SELECT * FROM ModelSubjectTime WHERE belowProgramID = :belowProgramID ORDER BY day ASC, timeStart ASC")
+    suspend fun getAllSubjectTimes(belowProgramID:String): List<ModelSubjectTime>
+
+    @Query("SELECT * FROM ModelSubjectTime WHERE id = :subjectTimeID")
+    suspend fun getSubjectTime(subjectTimeID:String)
+
+    @Query("SELECT * FROM ModelSubjectTime WHERE belowSubjectID = :belowSubjectID")
+    suspend fun getSubjectSubjectTime(belowSubjectID:String)
+
+    @Query("DELETE FROM ModelSubjectTime WHERE belowSubjectID = :belowSubjectID")
+    suspend fun deleteAllSubjectTimes(belowSubjectID: String)
+
+    @Query("DELETE FROM ModelSubjectTime WHERE id = :subjectTimeID")
+    suspend fun deleteSubjectTime(subjectTimeID: String)
+
+    @Query("UPDATE ModelSubjectTime SET " +
+            "day = :day, " +
+            "timeStart= :timeStart, " +
+            "timeFinish = :timeFinish, " +
+            "typeLesson = :typeTime, " +
+            "classRoom = :classRoom, " +
+            "reminderTime = :reminderTime, " +
+            "belowSubjectID =:belowSubjectID, " +
+            "belowProgramID = :belowProgramID  WHERE id = :id")
+    suspend fun updateSubjectTime(id: String,
+                                  day: String,
+                                  timeStart: String,
+                                  timeFinish: String,
+                                  typeTime: String,
+                                  classRoom: String,
+                                  reminderTime: String,
+                                  belowSubjectID: String,
+                                  belowProgramID: String)
+}
--- app/src/main/java/com/saboon/myprograms/ViewModels/SubjectVM/MainFragmentViewModel.kt
+++ app/src/main/java/com/saboon/myprograms/ViewModels/SubjectVM/MainFragmentViewModel.kt
@@ -3,11 +3,33 @@ package com.saboon.myprograms.ViewModels.SubjectVM
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import com.saboon.myprograms.Database.DatabaseMyPrograms
+import com.saboon.myprograms.Models.ModelProgram
 import com.saboon.myprograms.Models.ModelSubject
+import com.saboon.myprograms.Models.ModelSubjectTime
 import com.saboon.myprograms.ViewModels.BaseViewModel
 import kotlinx.coroutines.launch
 
 class MainFragmentViewModel(application: Application):BaseViewModel(application) {
+
+    val program = MutableLiveData<ModelProgram>()
+    val subjects = MutableLiveData<List<ModelSubject>?>()
+    val subjectTimes = MutableLiveData<List<ModelSubjectTime>?>()
+    val loading = MutableLiveData<Boolean>()
+    val empty = MutableLiveData<Boolean>()
+    val error = MutableLiveData<Boolean>()
+
+
+
+    fun getData(programID:String){
+        loading.value = true
+        launch {
+            program.value = DatabaseMyPrograms(getApplication()).programDAO().getProgram(programID)
+            subjects.value = DatabaseMyPrograms(getApplication()).subjectDAO().getAllSubjects()
+            subjectTimes.value
+
+
+        }
+    }
 
 
 }
