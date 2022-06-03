9b0c3dfb983e34217a32ead4082b837544871dcf
Sabahattin Çiçek <scicek12345@gmail.com>
2022-06-03 16:28:56 +0300

    designed fragment_add_edit_subject_time.xml

---
+++ app/src/main/java/com/saboon/myprograms/Fragments/SubjectProgram/AddEditSubjectTimeFragment.kt
@@ -0,0 +1,26 @@
+package com.saboon.myprograms.Fragments.SubjectProgram
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.saboon.myprograms.R
+
+
+class AddEditSubjectTimeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_edit_subject_time, container, false)
+    }
+
+}
